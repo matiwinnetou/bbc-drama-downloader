63599b330deeed4e1810cec5fdaef96813ac59a3
Mateusz Czeladka <mateusz.szczap@gmail.com>
2017-02-05 15:04:37 +0100

    logging updates

--- bbc-drama-downloader.rb
+++ bbc-drama-downloader.rb
@@ -1,13 +1,33 @@
 require 'rufus-scheduler'
 require 'open-uri'
 require 'simple-rss'
+require 'logger'
 
-scheduler = Rufus::Scheduler.new
-bbc_drama_feed_url = 'http://www.bbc.co.uk/programmes/p02nrv5m/episodes/downloads.rss'
-save_dir = ARGV[0]
+$scheduler = Rufus::Scheduler.new
+$bbc_drama_feed_url = 'http://www.bbc.co.uk/programmes/p02nrv5m/episodes/downloads.rss'
+$save_dir = Dir.pwd
+
+def create_logger
+  log = Logger.new(STDOUT)
+  log.level = Logger::INFO
+  log.progname = "bbc-drama-downloader"
+
+  log.formatter = proc do |severity, datetime, progname, msg|
+    date_format = datetime.strftime("%Y-%m-%d %H:%M:%S")
+    if severity == "INFO" or severity == "WARN"
+      "[#{date_format}] #{severity}  (#{progname}): #{msg}\n"
+    else
+      "[#{date_format}] #{severity} (#{progname}): #{msg}\n"
+    end
+  end
+
+  log
+end
+
+$log = create_logger
 
 def fetch_feed(save_dir, feed_url)
-  puts("Fetching feed: " + feed_url)
+  $log.info("Fetching feed: #{feed_url}")
 
   rss = SimpleRSS.parse open(feed_url)
   items = rss.items
@@ -26,26 +46,40 @@ end
 
 def store_file(url, saveFilePath)
   if (File.file?(saveFilePath))
-    puts "File already exists: " + saveFilePath
+    $log.warn("File already exists: #{saveFilePath}")
     return
   end
-  puts("Downloading: " + saveFilePath)
+  $log.info("Downloading: #{saveFilePath}")
   File.open(saveFilePath, "wb") do |saved_file|
     open(url, "rb") do |read_file|
       saved_file.write(read_file.read)
     end
   end
-  puts("Downloaded: " + saveFilePath)
+  $log.info("Downloaded: #{saveFilePath}")
 end
 
-if ARGV[1] == '-d'
-  fetch_feed(save_dir, bbc_drama_feed_url)
+$log.info("Starting up...")
+$log.info("Will save podcasts to: #{$save_dir}")
+
+if ARGV[0] == '-d'
+  $log.info("Immediate download selected")
+  fetch_feed($save_dir, $bbc_drama_feed_url)
+  $log.info("Downloaded and exiting...")
   exit 0
 end
 
 # 5 mins after midnight - everyday
-scheduler.cron('5 0 * * *') do
-  fetch_feed(save_dir, bbc_drama_feed_url)
+$scheduler.cron('0 * * * *') do |job|
+  begin
+    fetch_feed($save_dir, $bbc_drama_feed_url)
+    $log.info("Next time will be around: #{job.next_time}")
+    $log.info("Sleeping...")
+  rescue => e
+    $log.fatal("Unable to run job:")
+    $log.fatal(e)
+  end
 end
 
-scheduler.join
+$log.info("Scheduler join...")
+
+$scheduler.join
