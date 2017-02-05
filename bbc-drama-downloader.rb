3cd30f15f606e9bfe4bd6c5ba66b662b04367345
Mateusz Czeladka <mateusz.szczap@gmail.com>
2017-02-05 16:45:00 +0100

    improv

--- bbc-drama-downloader.rb
+++ bbc-drama-downloader.rb
@@ -2,10 +2,7 @@ require 'rufus-scheduler'
 require 'open-uri'
 require 'simple-rss'
 require 'logger'
-
-$scheduler = Rufus::Scheduler.new
-$bbc_drama_feed_url = 'http://www.bbc.co.uk/programmes/p02nrv5m/episodes/downloads.rss'
-$save_dir = Dir.pwd
+require 'optparse'
 
 def create_logger
   log = Logger.new(STDOUT)
@@ -25,6 +22,42 @@ def create_logger
 end
 
 $log = create_logger
+
+$options = {}
+$options[:immediate] = false
+$options[:save_dir] = Dir.pwd
+$options[:cron_entry] = '5 * * * *' # 5 mins after midnight - everyday
+
+$opt_parser = OptionParser.new do |opt|
+  opt.banner = "Usage: ruby bbc-drama-downloader.rb [OPTIONS]"
+  opt.separator  ""
+  opt.separator  "Options"
+  opt.separator  "     -i: Immediately start downloading, save and quit"
+  opt.separator  "     -p: Path to save to"
+  opt.separator  "     -c: Cron tab entry"
+
+  opt.on("-c", "--cron", "Cron entry") do |cron_entry|
+    $options[:cron_entry] = cron_entry
+  end
+
+  opt.on("-p=save_dir", "--path=save_dir", "Directory to where to store files") do |save_dir|
+    $options[:save_dir] = save_dir
+  end
+
+  opt.on("-i", "--immediately", "Download and exit") do
+    $options[:immediate] = true
+  end
+
+  opt.on("-h","--help", "help") do
+    puts $opt_parser
+  end
+
+end
+
+$opt_parser.parse!
+
+$scheduler = Rufus::Scheduler.new
+$bbc_drama_feed_url = 'http://www.bbc.co.uk/programmes/p02nrv5m/episodes/downloads.rss'
 
 def fetch_feed(save_dir, feed_url)
   $log.info("Fetching feed: #{feed_url}")
@@ -59,19 +92,20 @@ def store_file(url, saveFilePath)
 end
 
 $log.info("Starting up...")
-$log.info("Will save podcasts to: #{$save_dir}")
+$log.info("Will save podcasts to: #{$options[:save_dir]}")
 
-if ARGV[0] == '-d'
+if $options[:immediate]
   $log.info("Immediate download selected")
-  fetch_feed($save_dir, $bbc_drama_feed_url)
-  $log.info("Downloaded and exiting...")
+  fetch_feed($options[:save_dir], $bbc_drama_feed_url)
+  $log.info("Ran, did work and exiting...")
   exit 0
 end
 
-# 5 mins after midnight - everyday
-$scheduler.cron('0 * * * *') do |job|
+fetch_feed($options[:save_dir], $bbc_drama_feed_url)
+
+$scheduler.cron($options[:cron_entry]) do |job|
   begin
-    fetch_feed($save_dir, $bbc_drama_feed_url)
+    fetch_feed($options[:save_dir], $bbc_drama_feed_url)
     $log.info("Next time will be around: #{job.next_time}")
     $log.info("Sleeping...")
   rescue => e
